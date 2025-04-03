38825c7f5748fa8ebb3f89eca01e13865d7b2500
PhillY <philly706869@gmail.com>
2025-04-04 01:40:40 +0900

    png rendering logic added

--- build-logic.sh
+++ build-logic.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-/venv/bin/scour -i /mnt/origin.svg -o /mnt/build/output-scour.svg --enable-viewboxing --enable-id-stripping --enable-comment-stripping --shorten-ids --indent=none
-npx svgo -i /mnt/build/output-scour.svg -o /mnt/build/output.svg
-rm -rf /mnt/build/output-scour.svg
+cd /mnt/build
+/venv/bin/scour -i /mnt/origin.svg -o output-scour.svg --enable-viewboxing --enable-id-stripping --enable-comment-stripping --shorten-ids --indent=none
+npx svgo -i output-scour.svg -o output.svg
+rm -rf output-scour.svg
+inkscape output.svg -o 500x500.png --export-width=500 --export-height=500
--- build.Dockerfile
+++ build.Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:latest
 RUN apk update
-RUN apk add --update python3 py3-pip nodejs npm
+RUN apk add --update python3 py3-pip nodejs npm inkscape
 RUN python3 -m venv /venv
 RUN /venv/bin/pip install scour
 RUN npm i -g svgo
