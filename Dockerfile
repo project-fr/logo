0cf870756437f9abbbd41f3dd7ba2ef6a6824402
PhillY <philly706869@gmail.com>
2025-04-05 01:47:16 +0900

    removed docker

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM alpine:latest
-RUN apk add --update nodejs npm
-WORKDIR /build-logic
-COPY build-logic/index.js index.js
-COPY build-logic/svgo.config.mjs svgo.config.mjs
-COPY build-logic/package.json package.json
-RUN npm i
-ENTRYPOINT ["/bin/sh", "-c", "node index.js"]
--- build-logic/index.js
+++ build-logic/index.js
@@ -1,11 +1,16 @@
 import { spawn } from "child_process";
+import path from "node:path";
+
+const configPath = path.resolve("svgo.config.mjs");
+const sourcePath = path.resolve("../src");
+const outputPath = path.resolve("../build");
 
 spawn("npx", [
   "svgo",
   "--config",
-  "/build-logic/svgo.config.mjs",
+  configPath,
   "-rf",
-  "/app/src",
+  sourcePath,
   "-o",
-  "/app/build",
+  outputPath,
 ]);
